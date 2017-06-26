4f3bc8daa9d378721bd239bdc3bad80bd153e374
David Hayes <david@blackbricksoftware.com>
2017-06-26 15:47:03 -0500

    remove unnecessary reqire; now autoloaded by composer

--- CRM/Mailing/MailStore/MailgunDB.php
+++ CRM/Mailing/MailStore/MailgunDB.php
@@ -33,8 +33,8 @@
  *
  */
 
-require_once 'ezc/Base/src/ezc_bootstrap.php';
-require_once 'ezc/autoload/mail_autoload.php';
+// require_once 'ezc/Base/src/ezc_bootstrap.php';
+// require_once 'ezc/autoload/mail_autoload.php';
 class CRM_Mailing_MailStore_MailgunDB extends CRM_Mailing_MailStore {
 
   /**
